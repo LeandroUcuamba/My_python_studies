08b6b1df84a25a8b07e244797418112127a33712
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2024-12-30 22:14:30 +0100

    Initial commit

---
+++ 1-Variaveis/app.py
@@ -0,0 +1,11 @@
+
+nome = "Leandro"
+idade = 25
+gosto_de = "programação"
+altura = 1.81
+is_male = True | False
+
+
+print(f"Meu nome é {nome} .");
+print("Eu gosto de " + gosto_de + " .");
+print(f"Tenho {idade} anos de idade .");
---
+++ 2-trabalhando_com_numeros/app.py
@@ -0,0 +1,28 @@
+num1 = 5
+num2 = 3.5
+
+print(type(num1))
+print(type(num2))
+
+a = float(num1)
+print(a)
+print(type(a))
+
+b = int(num2)
+print(b)
+print(type(b))
+
+
+print(abs(-15))
+print(pow(4,4))
+print(max(23, 3.5, -34, 78))
+print(min(23, 3.5, -34, 78))
+print(round(8.8))
+
+print("################################")
+
+import math
+
+print(math.floor(8.9999))
+print(math.ceil(8.000001))
+print(math.sqrt(10))
